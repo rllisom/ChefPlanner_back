2e8e026714cbc9c66b73b2a5ae4d6074cc24c40d
Raúl Llinares <raullisom@gmail.com>
2026-02-13 08:26:30 +0100

    Inicio del proyecto 13/02

---
+++ chef_planner/src/main/java/com/salesianostriana/chefplanner/ChefPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.chefplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChefPlannerApplication.class, args);
+	}
+
+}
---
+++ chef_planner/src/test/java/com/salesianostriana/chefplanner/ChefPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.chefplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
