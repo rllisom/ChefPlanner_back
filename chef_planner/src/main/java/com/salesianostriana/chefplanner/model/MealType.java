5400c158a4bbb63976571c4b64ff62d1319c178f
Simón <chica.susim24@triana.salesianos.edu>
2026-02-16 19:59:38 +0100

    feat-08:model de MenuItem

---
+++ chef_planner/src/main/java/com/salesianostriana/chefplanner/model/MealType.java
@@ -0,0 +1,6 @@
+package com.salesianostriana.chefplanner.model;
+
+public enum MealType {
+
+    BREAKFAST, LUNCH, DINNER
+}
---
+++ chef_planner/src/main/java/com/salesianostriana/chefplanner/model/MenuItem.java
@@ -0,0 +1,41 @@
+package com.salesianostriana.chefplanner.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "menu_item", uniqueConstraints = {
+        @UniqueConstraint(name = "uk_user_date_mealtype", columnNames = {"user_id", "date", "meal_type"})
+})
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MenuItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "meal_type", nullable = false)
+    private MealType mealType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recipe_id", nullable = false)
+    private Recipe recipe;
+
+
+}
