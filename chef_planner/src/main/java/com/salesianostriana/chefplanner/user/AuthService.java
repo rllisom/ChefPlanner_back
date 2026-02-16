f9c8e9131a4dce15c075ce6352b1c577bc0daeb6
Pedromolvaz <pedromolvaz@gmail.com>
2026-02-16 22:14:19 +0100

    feat 00 hecho

---
+++ chef_planner/src/main/java/com/salesianostriana/chefplanner/user/AuthService.java
@@ -0,0 +1,17 @@
+package com.salesianostriana.chefplanner.user;
+
+import com.salesianostriana.chefplanner.user.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+
+}
---
+++ chef_planner/src/main/java/com/salesianostriana/chefplanner/user/model/User.java
@@ -0,0 +1,40 @@
+package com.salesianostriana.chefplanner.user.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false, unique=true)
+    private String email;
+
+    @Column(nullable=false, unique=true)
+    private String username;
+
+    @Column(nullable=false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(name="user_roles", joinColumns = @JoinColumn(name="user_id"))
+    @Column(name = "role")
+    private Set<UserRole> roles;
+
+
+
+}
