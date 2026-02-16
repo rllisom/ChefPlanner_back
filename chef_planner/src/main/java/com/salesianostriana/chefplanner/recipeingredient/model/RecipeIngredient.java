ec186f6a13e808894ccc7474a04dd8195c96b8dd
Raúl Llinares <raullisom@gmail.com>
2026-02-17 00:07:21 +0100

    feat-06.1 : Modelo y repositorio de la entidad Ingredient y RecipeIngredient v1

---
+++ chef_planner/src/main/java/com/salesianostriana/chefplanner/recipeingredient/model/RecipeIngredient.java
@@ -0,0 +1,40 @@
+package com.salesianostriana.chefplanner.recipeingredient.model;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class RecipeIngredient {
+
+    @EmbeddedId
+    private RecipeIngredientId id = new RecipeIngredientId();
+
+    private int quantity;
+    private String unit;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        RecipeIngredient that = (RecipeIngredient) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(id);
+    }
+}
