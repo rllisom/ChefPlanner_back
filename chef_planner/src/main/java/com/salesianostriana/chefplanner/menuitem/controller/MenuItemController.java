ab900982308200dad342d1ad73e3416b55606bb4
SimonSalesianos <chica.susim24@triana.salesianos.edu>
2026-02-17 14:10:34 +0100

    feature-08.1: creacion service, controller y model

--- chef_planner/src/main/java/com/salesianostriana/chefplanner/menuitem/controller/MenuItemController.java
+++ chef_planner/src/main/java/com/salesianostriana/chefplanner/menuitem/controller/MenuItemController.java
@@ -1,4 +1,20 @@
 package com.salesianostriana.chefplanner.menuitem.controller;
 
+import com.salesianostriana.chefplanner.menuitem.service.MenuItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/menuitem")
+@RequiredArgsConstructor
 public class MenuItemController {
+
+
+    private final MenuItemService menuItemService;
+
+
+
+
+
 }
--- chef_planner/src/main/java/com/salesianostriana/chefplanner/menuitem/model/MenuItem.java
+++ chef_planner/src/main/java/com/salesianostriana/chefplanner/menuitem/model/MenuItem.java
@@ -1,13 +1,14 @@
 package com.salesianostriana.chefplanner.menuitem.model;
 
-import com.salesianostriana.chefplanner.model.MealType;
+import com.salesianostriana.chefplanner.menuitem.model.MealType;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import org.apache.catalina.User;
 import java.time.LocalDate;
+import java.util.Set;
 
 @Entity
 @Table(name = "menu_item")
@@ -31,7 +32,7 @@ public class MenuItem {
 
     @Enumerated(EnumType.STRING)
     @Column(name = "meal_type", nullable = false)
-    private MealType mealType;
+    private Set<MealType> mealType;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "recipe_id", nullable = false)
