9fb631318754f98a423a81b8dc524c853f55d550
SimonSalesianos <chica.susim24@triana.salesianos.edu>
2026-02-18 12:54:05 +0100

    RF-07.4: Carpeta files con model y exception hecha

---
+++ chef_planner/src/main/java/com/salesianostriana/chefplanner/files/shared/exception/StorageException.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.chefplanner.files.shared.exception;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String msg) {
+        super(msg);
+    }
+
+    public StorageException(String msg, Exception e) {
+        super(msg, e);
+    }
+
+}
---
+++ chef_planner/src/main/java/com/salesianostriana/chefplanner/files/shared/model/FileMetadata.java
@@ -0,0 +1,17 @@
+package com.salesianostriana.chefplanner.files.shared.model;
+
+public interface FileMetadata {
+
+    String getFilename();
+    String getURL();
+    String getDeleteId();
+    String getDeleteURL();
+    String getId();
+    void setFilename(String filename);
+    void setURL(String url);
+    void setDeleteId(String deleteId);
+    void setDeleteURL(String deleteURL);
+    void setId(String id);
+
+
+}
