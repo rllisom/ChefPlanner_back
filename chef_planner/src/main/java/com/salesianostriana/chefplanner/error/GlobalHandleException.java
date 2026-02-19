d38c7fc43c0f99661dd5763881b07ea903022c27
Raúl Llinares <raullisom@gmail.com>
2026-02-19 17:15:08 +0100

    rf-07.2 : Carga de fichero en local terminado

--- chef_planner/src/main/java/com/salesianostriana/chefplanner/error/GlobalHandleException.java
+++ chef_planner/src/main/java/com/salesianostriana/chefplanner/error/GlobalHandleException.java
@@ -1,8 +1,10 @@
 package com.salesianostriana.chefplanner.error;
 
+import com.salesianostriana.chefplanner.files.shared.exception.StorageException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -28,6 +30,16 @@ public class GlobalHandleException extends ResponseEntityExceptionHandler {
 
         problemDetail.setTitle("Error en los argumentos");
         problemDetail.setType(URI.create("chefplanner.com/error/no-encontrado"));
+
+        return problemDetail;
+    }
+
+    @ExceptionHandler(StorageException.class)
+    public ProblemDetail handleStorageException(StorageException ex) {
+        ProblemDetail problemDetail =  ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+
+        problemDetail.setTitle("Error en el almacenamiento de ficheros");
+        problemDetail.setType(URI.create("chefplanner.com/error/storage-error"));
 
         return problemDetail;
     }
