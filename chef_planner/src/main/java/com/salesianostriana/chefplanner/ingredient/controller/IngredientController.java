7704be1191dd4f1daa8baafbd3398044317fc6ea
Pedromolvaz <pedromolvaz@gmail.com>
2026-02-23 20:19:19 +0100

    fix-14.2

--- chef_planner/src/main/java/com/salesianostriana/chefplanner/ingredient/controller/IngredientController.java
+++ chef_planner/src/main/java/com/salesianostriana/chefplanner/ingredient/controller/IngredientController.java
@@ -21,7 +21,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@RestControllerAdvice
+@RestController
 @RequestMapping("/api/v1")
 @Tag(name = "Endpoints de ingrediente" )
 public class IngredientController {
