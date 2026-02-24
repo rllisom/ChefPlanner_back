91249280cd26cbbc18bb0b9058cc46cd69a08a9b
Simón <chica.susim24@triana.salesianos.edu>
2026-02-24 20:21:18 +0100

    feature-37: User service modificado

--- chef_planner/src/main/java/com/salesianostriana/chefplanner/user/service/UserProfileService.java
+++ chef_planner/src/main/java/com/salesianostriana/chefplanner/user/service/UserProfileService.java
@@ -3,11 +3,11 @@ package com.salesianostriana.chefplanner.user.service;
 import com.salesianostriana.chefplanner.user.dto.UserListResponse;
 import com.salesianostriana.chefplanner.user.error.ProfileNotFoundException;
 import com.salesianostriana.chefplanner.user.model.User;
-import com.salesianostriana.chefplanner.user.model.UserProfile;
 import com.salesianostriana.chefplanner.user.repository.UserProfileRepository;
 import com.salesianostriana.chefplanner.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
@@ -19,22 +19,22 @@ public class UserProfileService {
     private final UserProfileRepository userProfileRepository;
     private final UserRepository userRepository;
 
+    @Transactional(readOnly = true)
     public List<UserListResponse> findAll() {
-       List<UserListResponse> response = userProfileRepository.findAll().stream()
-               .map( profile -> {
-                   User user = userRepository.findById(UUID.fromString(profile.getUserUuid()))
-                           .orElseThrow(
-                                   () -> new ProfileNotFoundException("No se encontró el perfil para el usuario con UUID: " + profile.getUserUuid())
-                           );
+        List<UserListResponse> response = userProfileRepository.findAll().stream()
+                .map(profile -> {
+                    User user = userRepository.findById(UUID.fromString(profile.getUserUuid()))
+                            .orElseThrow(
+                                    () -> new ProfileNotFoundException("No se encontró el perfil para el usuario con UUID: " + profile.getUserUuid())
+                            );
                     return UserListResponse.of(profile, user);
-               })
-               .toList();
+                })
+                .toList();
 
-       if (response.isEmpty()) {
-           throw new ProfileNotFoundException("No se encontraron perfiles de usuario.");
-       }
+        if (response.isEmpty()) {
+            throw new ProfileNotFoundException("No se encontraron perfiles de usuario.");
+        }
 
-       return response;
+        return response;
     }
-
 }
